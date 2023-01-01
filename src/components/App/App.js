d57a375705231c27eefd4931d5da04f33d597d4c
Danya Kovan <mr.kovan_2002@mail.ru>
2023-01-01 11:34:21 +0100

    +header for auth-ed users

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Header from '../Header/Header';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [ loggedIn, setLoggedIn ] = useState(true)
+
   return (
-    <Header />
+    <Header
+    loggedIn={loggedIn}
+    setLoggedIn={setLoggedIn}
+    />
   )
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import logo from '../../images/logo.svg'
+import profileIcon from '../../images/header-account-icon.svg'
 import { Link } from 'react-router-dom'
 import './Header.css'
 
-function Header() {
+function Header({ loggedIn }) {
 
     return (
         <header className="header">
@@ -10,16 +11,45 @@ function Header() {
                 <a href="https://yandex.ru" className="header__link" targer="_blank">
                     <img src={logo} alt="логотип" />
                 </a>
-                <nav className="header__nav">
-                    <Link 
-                    className="header__link header__link_type_registration" 
-                    to="signup"
-                    >Регистрация</Link>
-                    <Link 
-                    className="header__link header__link_type_authorization" 
-                    to="signin"
-                    >Войти</Link>
-                </nav>
+                <div className="header__nav-container">
+                    {!loggedIn ?
+                        <nav className="header__nav">
+                            <Link
+                                className="header__link header__link_type_registration"
+                                to="signup">
+                                Регистрация
+                            </Link>
+                            <Link
+                                className="header__link header__link_type_authorization"
+                                to="signin">
+                                Войти
+                            </Link>
+                        </nav>
+                        :
+                        <>
+                            <nav className="header__nav">
+                                <Link
+                                    className="header__link header__link_type_movies"
+                                    to="movies">
+                                    Фильмы
+                                </Link>
+                                <Link
+                                    className="header__link"
+                                    to="saved-movies">
+                                    Сохраненные фильмы
+                                </Link>
+                            </nav>
+                            <nav className="header__nav">
+                                <Link
+                                    className="header__link header__link_type_profile"
+                                    to="profile">
+                                    Аккаунт
+                                    <img src={profileIcon} alt="иконка профиля" className="header__profile-icon"></img>
+                                </Link>
+                            </nav>
+                        </>
+                    }
+                </div>
             </div>
         </header>
     )

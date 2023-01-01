820744324ffea5d759a0332526db0e6aab432e58
Danya Kovan <mr.kovan_2002@mail.ru>
2023-01-01 15:36:36 +0100

    +Navigation component +Header changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import logo from '../../images/logo.svg'
-import profileIcon from '../../images/header-account-icon.svg'
-import { Link } from 'react-router-dom'
 import './Header.css'
+import Navigation from '../Navigtion/Navigation'
 
 function Header({ loggedIn }) {
 
@@ -11,45 +10,9 @@ function Header({ loggedIn }) {
                 <a href="https://yandex.ru" className="header__link" targer="_blank">
                     <img src={logo} alt="логотип" />
                 </a>
-                <div className="header__nav-container">
-                    {!loggedIn ?
-                        <nav className="header__nav">
-                            <Link
-                                className="header__link header__link_type_registration"
-                                to="signup">
-                                Регистрация
-                            </Link>
-                            <Link
-                                className="header__link header__link_type_authorization"
-                                to="signin">
-                                Войти
-                            </Link>
-                        </nav>
-                        :
-                        <>
-                            <nav className="header__nav">
-                                <Link
-                                    className="header__link header__link_type_movies"
-                                    to="movies">
-                                    Фильмы
-                                </Link>
-                                <Link
-                                    className="header__link"
-                                    to="saved-movies">
-                                    Сохраненные фильмы
-                                </Link>
-                            </nav>
-                            <nav className="header__nav">
-                                <Link
-                                    className="header__link header__link_type_profile"
-                                    to="profile">
-                                    Аккаунт
-                                    <img src={profileIcon} alt="иконка профиля" className="header__profile-icon"></img>
-                                </Link>
-                            </nav>
-                        </>
-                    }
-                </div>
+                <Navigation 
+                loggedIn={loggedIn}
+                />
             </div>
         </header>
     )

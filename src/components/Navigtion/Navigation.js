657e9e33ae2a3757256f412fb7630a38f9abd3ee
Danya Kovan <mr.kovan_2002@mail.ru>
2023-01-01 15:44:59 +0100

    navigation dom renamed +navigation.css

--- src/components/Navigtion/Navigation.js
+++ src/components/Navigtion/Navigation.js
@@ -1,50 +1,51 @@
 import { Link } from 'react-router-dom'
 import profileIcon from '../../images/header-account-icon.svg'
 import '../Header/Header.css'
+import './Navigation.css'
 
 function Navigation({ loggedIn }) {
 
     return (
-        <div className="header__nav-container">
-                    {!loggedIn ?
-                        <nav className="header__nav">
-                            <Link
-                                className="header__link header__link_type_registration"
-                                to="signup">
-                                Регистрация
-                            </Link>
-                            <Link
-                                className="header__link header__link_type_authorization"
-                                to="signin">
-                                Войти
-                            </Link>
-                        </nav>
-                        :
-                        <>
-                        <button type="button" className="header__burger-button header__burger-button_inactive"></button>
-                            <nav className="header__nav">
-                                <Link
-                                    className="header__link header__link_type_movies"
-                                    to="movies">
-                                    Фильмы
-                                </Link>
-                                <Link
-                                    className="header__link"
-                                    to="saved-movies">
-                                    Сохраненные фильмы
-                                </Link>
-                            </nav>
-                            <nav className="header__nav">
-                                <Link
-                                    className="header__link header__link_type_profile"
-                                    to="profile">
-                                    Аккаунт
-                                    <img src={profileIcon} alt="иконка профиля" className="header__profile-icon"></img>
-                                </Link>
-                            </nav>
-                        </>
-                    }
-                </div>
+        <div className="navigation">
+            {!loggedIn ?
+                <nav className="navigation__nav">
+                    <Link
+                        className="navigation__link navigation__link_type_registration"
+                        to="signup">
+                        Регистрация
+                    </Link>
+                    <Link
+                        className="navigation__link navigation__link_type_authorization"
+                        to="signin">
+                        Войти
+                    </Link>
+                </nav>
+                :
+                <>
+                    <button type="button" className="navigation__burger-button"></button>
+                    <nav className="navigation__nav">
+                        <Link
+                            className="navigation__link navigation__link_type_movies"
+                            to="movies">
+                            Фильмы
+                        </Link>
+                        <Link
+                            className="navigation__link"
+                            to="saved-movies">
+                            Сохраненные фильмы
+                        </Link>
+                    </nav>
+                    <nav className="navigation__nav">
+                        <Link
+                            className="navigation__link navigation__link_type_profile"
+                            to="profile">
+                            Аккаунт
+                            <img src={profileIcon} alt="иконка профиля" className="header__profile-icon"></img>
+                        </Link>
+                    </nav>
+                </>
+            }
+        </div>
     )
 }
 

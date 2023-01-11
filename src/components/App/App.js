2ea92e1f7125747ed103b08fd4e3b967f9b8f7b2
Danya Kovan <mr.kovan_2002@mail.ru>
2023-01-11 17:24:40 +0100

    +protectedoute structure +mini refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
 
 function App() {
 
@@ -13,29 +14,28 @@ function App() {
     setIsBurgerOpened(!isBurgerOpened)
   }
 
-  /*
-
-  const renderHeaderRoutes = ({ paths, element }) =>
-    paths.map((path) => <Route key={path} path={path} element={element} />); // в react router 6 в path нельзя передать массив 
-  // чтобы не писать Route 4 раза, можно написать функцию
-
-  */
- 
   return (
     <>
       <Routes>
 
-        <Route path="/" element={
-        <Main
-        loggedIn={loggedIn}
-        setLoggedIn={setLoggedIn}
-        handleOnClickBurger={handleOnClickBurger}
-        isBurgerOpened={isBurgerOpened}
-        />
+        <Route path="/" exact element={
+          <Main
+            loggedIn={loggedIn}
+            setLoggedIn={setLoggedIn}
+            handleOnClickBurger={handleOnClickBurger}
+            isBurgerOpened={isBurgerOpened}
+          />
         } />
 
+        {
+          loggedIn ?
+            <Route path="/movies" element={<Movies />} /> :
+            <Route path="/movies" element={<Navigate to="/signup" />} />
+        }
+
+
       </Routes>
-    </>
+    </> 
   )
 }
 
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,10 @@
+import './Movies.css'
+
+function Movies () {
+    
+    return (
+        <h3 className='movies'>MOVIES</h3>
+    )
+}
+
+export default Movies

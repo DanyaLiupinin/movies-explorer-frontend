a084e2f2b71bb91e2b6ea496cda32bc8bef23aa3
Danya Kovan <mr.kovan_2002@mail.ru>
2023-01-14 14:40:39 +0100

    +NotFound 1280px & 768px

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import './NotFound.css'
+import { useNavigate } from 'react-router-dom'
 
 function NotFound() {
+
+    const navigate = useNavigate()
+
     return (
         <section className='notFound'>
             <h1 className="notFound__error">404</h1>
             <h2 className="notFound__error-text">Страница не найдена</h2>
+            <button className="notFound__navigate" onClick={() => navigate(-1)}>Назад</button>
         </section>
     )
 }

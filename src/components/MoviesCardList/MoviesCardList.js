b15bae0db18e09fb4bb64d7b3f65efc35d736d48
Danya Kovan <mr.kovan_2002@mail.ru>
2023-01-16 15:58:10 +0100

    +MovieCardList states for upload films

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -8,29 +8,36 @@ function MoviesCardList({ allMovies, setAllMovies, setSavedMovies }) {
     const [width, setWidth] = useState(window.innerWidth) // ширина окна
 
     const numberOfVisibleFilms = (width) => {
-        if (width >= 1280) {
+        if (width >= 1051) {
             return 16;
-        } else if (width >= 768) {
+        } else if (width >= 769) {
+            return 12;
+        } else if (width >= 451) {
             return 8;
         } else {
             return 5;
         }
     }
 
-    const [movieCounter, setMovieCounter] = useState(numberOfVisibleFilms(width))
-    const [visibleMovies, setVisibleMovies] = useState(allMovies.slice(0, movieCounter))
+    const showMoreStep = (width) => {
+        if (width >= 1051) {
+            return 4;
+        } else if (width >= 769) {
+            return 3;
+        } else if (width >= 451) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 
-    
-    useEffect(() => {
-
-        setWidth(window.innerWidth)
-
-    }, [width])
-    
-
+    const [movieCounter, setMovieCounter] = useState(numberOfVisibleFilms(width)) // в заивимсоти от ширины устанавливаем кол-во видимых фильмов
+    const [movieStep, setMovieStep] = useState(showMoreStep(width)) // сколько фильмов появится при 'показать ещё'
+                                                                 // надо ли для этого стейт? или просто переменной  достаточно? 
+    const [visibleMovies, setVisibleMovies] = useState(allMovies.slice(0, movieCounter)) // загружаем в стейт видимые фильмы
 
     function showMoreHandler() {
-        setMovieCounter(movieCounter + 4) // разобраться почему карточки рендерятся только после второго клика
+        setMovieCounter(movieCounter + movieStep) // разобраться почему карточки рендерятся только после второго клика
         // перенести рендер начального массива карточек в useeffect
 
         setVisibleMovies(allMovies.slice(0, movieCounter))
